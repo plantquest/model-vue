dfa0a3b5513f1232191b0644c4784d1132e98266
mohitnimavatgh <mohitnimavatgh@gmail.com>
2024-12-13 12:32:21 +0530

    when we go in navigation mode assets should be clear

--- src/components/BasicSide.vue
+++ src/components/BasicSide.vue
@@ -50,7 +50,7 @@
   <div v-if="!showSearch2">
         <img :src="`${publicPath}Layer_5.svg`" alt="Layer_5" class="Layer_5"
         style="position:absolute; z-index:1; margin:10px 0; margin-left:16px"
-        @click="showSearch2 = true"
+        @click="handleNavigationClick"
         
          />
 
@@ -500,6 +500,11 @@ export default {
       const temp = this.search;
       this.search = this.search2;
       this.search2 = temp;
+    },
+
+    handleNavigationClick(){
+      this.showSearch2 = true
+      this.$store.dispatch('vxg_trigger_assets_clear');
     },
  
 

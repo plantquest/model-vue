cf29b87b82c176ec7ca23230a8eb1e73aecf9e2d
Chayma-ab <abidallicha@gmail.com>
2025-02-26 16:42:25 +0100

    stagesNav expanded by default

--- src/components/BasicNavStages.vue
+++ src/components/BasicNavStages.vue
@@ -45,7 +45,7 @@ export default {
     data() {
         return {
             showNav: true,
-            isExpanded: false,
+            isExpanded: 0,
             iconSrc: 'nav_in.svg', // Initial icon
             publicPath: process.env.BASE_URL || '/',
             pathData: null ,// Add a data property to store the pathData

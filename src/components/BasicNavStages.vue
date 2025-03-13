b3bf899c6f3cea199cf558f63a3059debe34e529
mohitnimavatgh <mohitnimavatgh@gmail.com>
2025-03-13 11:18:42 +0530

    not all stages are showing

--- src/components/BasicNavStages.vue
+++ src/components/BasicNavStages.vue
@@ -216,7 +216,7 @@ methods: {
                       j++;
                   }
                   // first node type Standar (j)
-                  if(j < steps.length-3){
+                  if(j < steps.length){
                       msg += `${this.levelNames[steps[j].map]}`
                       messages.push({msg, map: steps[i].map-1});
                   }

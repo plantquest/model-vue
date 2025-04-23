034811e7a0aec8751fb685bd2573268de7f7bfe0
Kieran Desmond <34133202+kierandesmond@users.noreply.github.com>
2025-04-23 10:11:58 +0200

    Revert "v0.18.96-pqview-stable"
    This reverts commit 0d4f1db3aec6b42626fb16d8f85756b38ad4bf12.

--- vue.config.js
+++ vue.config.js
@@ -1,75 +1,18 @@
-/* vue.config.js – optimised for Vue 2 + Vuetify */
-const CompressionPlugin = require('compression-webpack-plugin') // gzip
-const { VueLoaderPlugin } = require('vue-loader')
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
-const TerserPlugin = require('terser-webpack-plugin')
-
 module.exports = {
-  /* 1.  Don't ship the template compiler in production
-        (≈10 kB gzipped).  Keep it in dev because it's handy.          */
-  runtimeCompiler: process.env.NODE_ENV !== 'production',
+  runtimeCompiler: true,
 
-  /* 2.  Disable source-maps in prod (-20 kB‒60 kB).                   */
-  productionSourceMap: false,
-
-  /* 3.  Extract CSS, but also drop duplicated rules.                  */
   css: {
-    extract: { ignoreOrder: true },
+    extract: true,
   },
 
-  /* 4.  One single entry, but let Vue CLI create vendor/common chunks
-        so the browser can cache them.*                                */
   pages: {
-    index: {
-      entry: 'src/index.js',
-      chunks: ['chunk-vendors', 'chunk-common', 'index'],
-    },
+    index: 'src/index.js',
   },
-
-  /* 5.  Anything you load from a CDN shouldn't be in the bundle.      */
-  configureWebpack: (env) => ({
-    externals: env.production
-      ? {
-          vue: 'Vue',
-          vuetify: 'Vuetify',
-        }
-      : {},
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          terserOptions: {
-            compress: { drop_console: true },
-            output: { comments: false },
-          },
-          extractComments: false,
-        }),
-      ],
-      splitChunks: {
-        chunks: 'all',
-        maxSize: 240 * 1024,
-      },
-    },
-    plugins: [
-      new CompressionPlugin({
-        test: /\.(js|css|html|svg)$/,
-        algorithm: 'gzip',
-        compressionOptions: { level: 9 },
-        threshold: 10 * 1024,
-        minRatio: 0.8,
-      }),
-    ],
-  }),
-
-  /* Dev server settings */
+  configureWebpack: {
+    externals: /vuetify\/lib/,
+  },
   devServer: {
+    // 'Access-Control-Allow-Origin': '*',
     port: 19191,
   },
-
-  /* Add vue-loader configuration */
-  chainWebpack: (config) => {
-    config.module.rule('vue').use('vue-loader').loader('vue-loader')
-  },
-
-  transpileDependencies: ['vuetify'],
 }

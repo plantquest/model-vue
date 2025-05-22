bc05ec25fc80abd8adb3d0526423b1390eadeb21
Chayma-ab <abidallicha@gmail.com>
2025-05-22 15:20:57 +0100

    save changes

--- src/components/BasicSide.vue
+++ src/components/BasicSide.vue
@@ -49,7 +49,7 @@
                style="max-width:200px;display:inline-block;margin-left:48%;text-transform: none;font-size:12px; color: #fff;top:10px"
             class="btn-clear"
           @click="clearFilter(); "
-          >{{ showSearch2 ? 'Close Navigation Mode' : 'Clearr Search' }}</v-btn>
+          >{{ showSearch2 ? 'Close Navigation Mode' : 'Clear Search' }}</v-btn>
 
 
 <div v-if="$route.name == 'pqview'">

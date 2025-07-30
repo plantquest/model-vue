d248bf0b7f20574100512e9578247b99e0ce2809
mohitnimavatgh <mohitnimavatgh@gmail.com>
2025-07-30 16:49:16 +0530

    clear search does not clear search value

--- src/components/BasicSide.vue
+++ src/components/BasicSide.vue
@@ -411,6 +411,9 @@ export default {
 
    '$store.state.trigger.search.a' (term) {
       this.search = term
+      if (term == '') {
+        return 
+      }
       if(term == '' && this.$refs.search) {
         this.$refs.search.reset()
         this.tag_items = this.items.map(tag_alias)

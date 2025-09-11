e2d997e7962a353912845ab8004ac1ea2592bf78
mohitnimavatgh <129922958+mohitnimavatgh@users.noreply.github.com>
2025-09-11 16:23:26 +0530

    Change Add Fixed Asset to Add Asset (#97)

--- src/components/BasicHead.vue
+++ src/components/BasicHead.vue
@@ -687,7 +687,7 @@
       <v-icon left medium>
         mdi-map-marker-path
       </v-icon>
-      Add {{ itemName == 'Asset' ? 'Fixed Asset' : itemName }}
+      Add {{ itemName == 'Asset' ? 'Asset' : itemName }}
     </v-btn>
   
     <v-icon

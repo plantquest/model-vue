9624ede907f013c4215a83e2449d2aa439cce5fb
mohitnimavatgh <mohitnimavatgh@gmail.com>
2025-09-12 16:00:44 +0530

    Add Asset Should Say Assign Asset Location

--- src/components/BasicHead.vue
+++ src/components/BasicHead.vue
@@ -687,7 +687,8 @@
       <v-icon left medium>
         mdi-map-marker-path
       </v-icon>
-      Add {{ itemName == 'Asset' ? 'Asset' : itemName }}
+      <span v-if="selectedSap !== 'SAP PM Assets'">Add {{ itemName == 'Asset' ? 'Asset' : itemName }}</span>
+      <span v-else>Assign Asset Code</span>
     </v-btn>
   
     <v-icon
